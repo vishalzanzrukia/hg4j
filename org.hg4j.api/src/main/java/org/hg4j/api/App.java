3fd66e98ed0a0d9f7276a44d1acfdda947970268
VishalZanzrukia <vishalzanzrukia@gmail.com>
2015-08-11 00:40:26 +0530

    Initial Commit

---
+++ org.hg4j.api/src/main/java/org/hg4j/api/App.java
@@ -0,0 +1,13 @@
+package org.hg4j.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
